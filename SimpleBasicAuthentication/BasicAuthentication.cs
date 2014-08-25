acbee9ab6a4ee258643cc360a373c727d3c97357
David Sinclair <david@icebit.se>
2014-08-25 13:50:39 +0200

    Initial commit

---
+++ SimpleBasicAuthentication/BasicAuthentication.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBasicAuthentication
+{
+    public static class BasicAuthentication
+    {
+        public static AuthenticationHeaderValue Create(string username, string password)
+        {
+            string credentials = string.Format("{0}:{1}", username, password);
+            var byteCredentials = Encoding.ASCII.GetBytes(credentials);
+            var base64Credentials = Convert.ToBase64String(byteCredentials);
+
+            return new AuthenticationHeaderValue("Basic", base64Credentials);
+        }
+    }
+}
